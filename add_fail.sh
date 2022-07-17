b41748faa7de720988b3c0e40088d54bf6b2e058
Maksim_Bilyi <w0957316192@gmail.com>
2022-07-17 03:10:11 +0300

    add check email

--- add_fail.sh
+++ add_fail.sh
@@ -7,3 +7,4 @@ echo "№2 15.07.22 w0957316192@gmail.com" > Bilyi/varj/gr1/Maksym_Bilyi.txt
 echo "№2 15.07.22 w0957316192@gmail.com" > Bilyi/vari/gr2/Maksym_Bilyi.txt
 cp "Bilyi/varj/gr1/Maksym_Bilyi.txt" "Bilyi/Maksym_Bilyi.txt";
 cp "Bilyi/vari/gr2/Maksym_Bilyi.txt" "Bilyi/Maksym_Bilyi1.txt";
+grep  '[[:alnum:]+\.\_\-]*@[[:alnum:]+\.\_\-]*' Bilyi/*.txt
