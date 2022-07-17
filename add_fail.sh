3670935465f57aca8a06aab84dfb9d0000c16fcd
Maksim_Bilyi <w0957316192@gmail.com>
2022-07-17 03:55:04 +0300

    ver 6

--- add_fail.sh
+++ add_fail.sh
@@ -8,3 +8,6 @@ echo "№2 15.07.22 w0957316192@gmail.com" > Bilyi/vari/gr2/Maksym_Bilyi.txt
 cp "Bilyi/varj/gr1/Maksym_Bilyi.txt" "Bilyi/Maksym_Bilyi.txt";
 cp "Bilyi/vari/gr2/Maksym_Bilyi.txt" "Bilyi/Maksym_Bilyi1.txt";
 grep  '[[:alnum:]+\.\_\-]*@[[:alnum:]+\.\_\-]*' Bilyi/*.txt
+
+
+
