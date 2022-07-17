9a43b5e647f0ed4537e8eb5bfcfd728109053e15
Maksim_Bilyi <w0957316192@gmail.com>
2022-07-17 03:56:29 +0300

    ver 7

--- add_fail.sh
+++ add_fail.sh
@@ -3,11 +3,8 @@
 mkdir -p Bilyi/varj/gr1
 mkdir -p Bilyi/vari/gr2
 mkdir -p Bilyi/varg/
-echo "№2 15.07.22 w0957316192@gmail.com" > Bilyi/varj/gr1/Maksym_Bilyi.txt
-echo "№2 15.07.22 w0957316192@gmail.com" > Bilyi/vari/gr2/Maksym_Bilyi.txt
+echo "№2 16.07.22 w0957316192@gmail.com" > Bilyi/varj/gr1/Maksym_Bilyi.txt
+echo "№2 16.07.22 w0957316192@gmail.com" > Bilyi/vari/gr2/Maksym_Bilyi.txt
 cp "Bilyi/varj/gr1/Maksym_Bilyi.txt" "Bilyi/Maksym_Bilyi.txt";
 cp "Bilyi/vari/gr2/Maksym_Bilyi.txt" "Bilyi/Maksym_Bilyi1.txt";
 grep  '[[:alnum:]+\.\_\-]*@[[:alnum:]+\.\_\-]*' Bilyi/*.txt
-
-
-
